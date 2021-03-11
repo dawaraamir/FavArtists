893d199ca51055a1b51dd4d60d0e3293aa367a29
Dawar Aamir <dawar.aamir@gmail.com>
2021-03-11 17:41:05 -0500

    This is for HW 2

---
+++ src/main/java/favartists/Top3.java
@@ -0,0 +1,12 @@
+package favartists;
+
+public class Top3 {
+    public static void main(String[] args) {
+        String artist1 = "50 Cent";
+        String artist2 = "Dave East";
+        String artist3 = "Chris Brown";
+
+        System.out.println("My top 3 artists are: "+ artist1 + ", "+ artist2 + ", & " + artist3);
+
+    }
+}
